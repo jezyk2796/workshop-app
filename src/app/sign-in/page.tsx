4cf8f1faa88bd968c80b83d64fca96692e841940
Szymon <jezyk2796@op.pl>
2025-10-28 23:06:55 +0100

    feat: login functionality on frontend side done

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -12,34 +12,82 @@ import {
   Typography,
 } from "@mui/material";
 import * as styles from "./sign-in.styles";
+import { useForm } from "react-hook-form";
+import { SignInInputType, signInSchema } from "@/lib/schemas/sign-in.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import { axiosClient } from "@/lib/api/axios";
+import { toast } from "react-toastify";
+import { AxiosError } from "axios";
 
 const metadata: Metadata = {
   title: "Sign In",
 };
 
 const SignInPage = () => {
+  const router = useRouter();
+  const { register, handleSubmit, formState } = useForm<SignInInputType>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      rememberUser: false,
+    },
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (data: SignInInputType) => {
+      const response = await axiosClient.post("/auth/sign-in", data);
+      return response.data;
+    },
+    onSuccess: () => {
+      toast.success("Logged in ;) Welcome!");
+      setTimeout(() => {
+        router.push("/dashboard");
+      }, 1000);
+    },
+    onError: (error) => {
+      const err = error as AxiosError<{ error: string }>;
+      const message = err.response?.data?.error || "Invalid email or password";
+      toast.error(message);
+    },
+  });
+
+  const onSubmit = (data: SignInInputType) => mutate(data);
+
   return (
     <Box sx={styles.signInBox}>
       <Paper sx={styles.signInContainer}>
         <Typography variant="h1" sx={{ mb: 1 }}>
           Sign In
         </Typography>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Box sx={styles.formContainer}>
-            <TextField placeholder="Email *" variant="standard" sx={styles.signInTextField} />
+            <TextField
+              placeholder="Email *"
+              variant="standard"
+              sx={styles.signInTextField}
+              helperText={formState.errors.email?.message?.toString()}
+              error={formState.errors.email && true}
+              {...register("email")}
+            />
             <TextField
               placeholder="Password *"
               type="password"
               variant="standard"
               sx={styles.signInTextField}
+              helperText={formState.errors.password?.message?.toString()}
+              error={formState.errors.password && true}
+              {...register("password")}
             />
             <FormControlLabel
-              control={<Checkbox sx={styles.signInCheckbox} />}
+              control={<Checkbox sx={styles.signInCheckbox} {...register("rememberUser")} />}
               label="Remember me"
               sx={{ mt: 1 }}
             />
             <Box sx={{ display: "flex" }}>
-              <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+              <Button type="submit" variant="contained" sx={{ mt: 2 }} disabled={isPending}>
                 Sign in
               </Button>
             </Box>
